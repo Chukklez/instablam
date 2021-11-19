17cce6204ba15ad68ee5b67517afb8df23adcb2d
Bastian <bastian.bjorn@gmail.com>
2021-11-19 19:13:14 +0100

    created webcam component and installed webcam from react-webcam and made it work

---
+++ src/components/Webcam.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Webcam from "react-webcam"
+
+function webcam() {
+    return (
+        <>
+        <Webcam 
+        height={480}
+        width={480}
+        />
+        </>
+    )
+
+}
+
+export default webcam
